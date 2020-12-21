cc22635c80a0447a19c7654e9f08536d87b87178
DivyaKalash <67993272+DivyaKalash@users.noreply.github.com>
2020-12-21 23:14:58 +0530

    Update main.py

--- Coffee_Machine/main.py
+++ Coffee_Machine/main.py
@@ -1,13 +1,14 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from art import logo
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 
 is_on = True
-
+print(logo)
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
